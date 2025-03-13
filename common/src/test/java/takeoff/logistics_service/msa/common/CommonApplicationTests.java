d5c9b7a71fdcf24416ef5e64055d0624491f8d76
hyezuu <duudu0704@gmail.com>
2025-03-13 15:14:44 +0900

    feature/#3: 공통 모듈 추가

---
+++ common/src/test/java/takeoff/logistics_service/msa/common/CommonApplicationTests.java
@@ -0,0 +1,13 @@
+package takeoff.logistics_service.msa.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
