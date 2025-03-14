ca71597d8fcdc013ebf358126c97782154c72510
hyezuu <duudu0704@gmail.com>
2025-03-15 06:42:34 +0900

    fix/10: StockInternalController - abortStock 수정

--- product/src/main/java/takeoff/logistics_service/msa/product/stock/presentation/internal/StockInternalController.java
+++ product/src/main/java/takeoff/logistics_service/msa/product/stock/presentation/internal/StockInternalController.java
@@ -1,9 +1,6 @@
 package takeoff.logistics_service.msa.product.stock.presentation.internal;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
