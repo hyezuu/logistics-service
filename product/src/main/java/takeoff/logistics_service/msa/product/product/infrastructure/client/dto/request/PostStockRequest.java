adb3ea7c050af0f2c5350e2ae0b5dcdb79b0fac2
hyezuu <duudu0704@gmail.com>
2025-03-17 18:37:36 +0900

    feature/28: rename StockIdDto

--- product/src/main/java/takeoff/logistics_service/msa/product/product/infrastructure/client/dto/request/PostStockRequest.java
+++ product/src/main/java/takeoff/logistics_service/msa/product/product/infrastructure/client/dto/request/PostStockRequest.java
@@ -1,13 +1,13 @@
 package takeoff.logistics_service.msa.product.product.infrastructure.client.dto.request;
 
 import takeoff.logistics_service.msa.product.product.application.dto.request.PostStockRequestDto;
-import takeoff.logistics_service.msa.product.product.infrastructure.client.dto.StockIdDto;
 
-public record PostStockRequest(StockIdDto stockId, Integer quantity) {
+public record PostStockRequest(StockIdRequest stockIdResponse, Integer quantity) {
 
 	public static PostStockRequest from(PostStockRequestDto requestDto) {
 		return new PostStockRequest(
-			StockIdDto.from(requestDto.productId(), requestDto.hubId()), requestDto.quantity());
+			StockIdRequest
+				.from(requestDto.productId(), requestDto.hubId()), requestDto.quantity());
 	}
 }
 
--- product/src/main/java/takeoff/logistics_service/msa/product/product/infrastructure/client/dto/response/PostStockResponse.java
+++ product/src/main/java/takeoff/logistics_service/msa/product/product/infrastructure/client/dto/response/PostStockResponse.java
@@ -2,14 +2,13 @@ package takeoff.logistics_service.msa.product.product.infrastructure.client.dto.
 
 import java.time.LocalDateTime;
 import takeoff.logistics_service.msa.product.product.application.dto.response.PostStockResponseDto;
-import takeoff.logistics_service.msa.product.product.infrastructure.client.dto.StockIdDto;
 
-public record PostStockResponse(StockIdDto stockId, Integer quantity, LocalDateTime createdAt) {
+public record PostStockResponse(StockIdResponse stockIdResponse, Integer quantity, LocalDateTime createdAt) {
 
 	public PostStockResponseDto toApplicationDto(){
 		return PostStockResponseDto.builder()
-			.productId(stockId.productId())
-			.hubId(stockId.hubId())
+			.productId(stockIdResponse.productId())
+			.hubId(stockIdResponse.hubId())
 			.quantity(quantity)
 			.createdAt(createdAt).build();
 	}
