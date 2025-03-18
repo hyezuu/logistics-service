1bde52ebc0879d204cba2f26b71fb5df801812e5
hyezuu <duudu0704@gmail.com>
2025-03-18 21:01:16 +0900

    refactor/42: 사용하지 않는 필드 제거

--- product/src/main/java/takeoff/logistics_service/msa/product/stock/application/dto/StockSearchCondition.java
+++ product/src/main/java/takeoff/logistics_service/msa/product/stock/application/dto/StockSearchCondition.java
@@ -6,14 +6,10 @@ public record StockSearchCondition(UUID productId,
 								   UUID hubId,
 								   Boolean isAsc,
 								   String sortBy
-//	,
-//								   int page,
-//								   int size
 								   ) {
 	public StockSearchCondition {
 		isAsc = isAsc != null ? isAsc : false;
 		sortBy = sortBy != null ? sortBy : "createdAt";
-//		size = (page == 10 || page == 30 || page == 50) ? page : 10;
 	}
-	// domain 계층으로의 변환 필요
+
 }
