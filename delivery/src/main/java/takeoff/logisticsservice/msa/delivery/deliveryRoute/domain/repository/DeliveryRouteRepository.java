f0760ec767b00fca5a518abfe2abdc0432600786
sodychoe <sodychoe@icloud.com>
2025-03-21 09:24:00 +0900

    feautre/#81 : DeliveryRoute Infra 계층 작성

--- delivery/src/main/java/takeoff/logisticsservice/msa/delivery/deliveryRoute/domain/repository/DeliveryRouteRepository.java
+++ delivery/src/main/java/takeoff/logisticsservice/msa/delivery/deliveryRoute/domain/repository/DeliveryRouteRepository.java
@@ -1,8 +1,15 @@
 package takeoff.logisticsservice.msa.delivery.deliveryRoute.domain.repository;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
 import takeoff.logisticsservice.msa.delivery.deliveryRoute.domain.entity.DeliveryRoute;
 
 public interface DeliveryRouteRepository {
 
   DeliveryRoute save(DeliveryRoute deliveryRoute);
+
+  Collection<DeliveryRoute> saveAll(Collection<DeliveryRoute> deliveryRoutes);
+
+  List<DeliveryRoute> findAllByDeliveryId(UUID deliveryId);
 }
