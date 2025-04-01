46ed2e1938b72aaf453b8ecc7f6a3a150e6d92ec
hyezuu <duudu0704@gmail.com>
2025-04-02 03:21:31 +0900

    rename/#0: TopicConfig 설정

---
+++ product/src/main/java/takeoff/logistics_service/msa/product/stock/infrastructure/kafka/StockTopicConfig.java
@@ -0,0 +1,33 @@
+package takeoff.logistics_service.msa.product.stock.infrastructure.kafka;
+
+import java.util.Map;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+@ComponentScan("takeoff.logistics_service.msa.product.stock")
+public class StockTopicConfig {
+
+	private static final String STOCK_TOPIC_NAME = "stock-events";
+
+	@Bean
+	public NewTopic stockTopic() {
+		return TopicBuilder.name(STOCK_TOPIC_NAME)
+			.partitions(3)
+			.replicas(1)
+			.configs(Map.of(
+				// 해당 토픽의 보존 기간을 지정
+				"retention.ms", "604800000",
+				"cleanup.policy", "delete"
+			))
+			.build();
+	}
+	@Bean
+	public String getStockTopic() {
+		return STOCK_TOPIC_NAME;
+	}
+}
+
